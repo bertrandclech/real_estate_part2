6402eaa11ebf56c0da944f4e0a1602c800a9bfbf
Gwigzz <jhonguetter199@hotmail.fr>
2022-03-07 17:16:03 +0100

    update profil

--- user/profil.php
+++ user/profil.php
@@ -19,5 +19,11 @@ require_once '../templates/header.php'; ?>
 
 <h1>Votre profil</h1>
 
+<p>Prénom : <b><?= $_SESSION['auth']['prenom']?></b></p>
+<p>Email : <b><?= $_SESSION['auth']['email']?></b></p>
+<p>Date inscrption : <b><?= $_SESSION['auth']['date']?></b></p>
+
+
+
 
 <?php require_once '../templates/footer.php'; ?>
